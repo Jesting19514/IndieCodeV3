02943182ce9a1f7a8b6a73ec32c36874af144a60
Jesting19514 <151665332+Jesting19514@users.noreply.github.com>
2024-09-02 08:35:12 -0600

    ups

--- src/pruebas/AlertaApp.java
+++ src/pruebas/AlertaApp.java
@@ -1,9 +1,6 @@
 package pruebas;
-
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
