8ce53045f0f0c211e687b5a63b8ddf35a262a4f0
Jesting19514 <151665332+Jesting19514@users.noreply.github.com>
2024-09-03 07:53:40 -0600

    no se que estoy haciendo

--- src/pruebas/AlertaApp.java
+++ src/pruebas/AlertaApp.java
@@ -2,8 +2,6 @@ package pruebas;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
@@ -26,12 +24,9 @@ public class AlertaApp {
     private static class Alerta {
         String nombreContrato;
         Date fechaTermino;
-        TimerTask tarea;
-
         Alerta(String nombreContrato, Date fechaTermino, TimerTask tarea) {
             this.nombreContrato = nombreContrato;
             this.fechaTermino = fechaTermino;
-            this.tarea = tarea;
         }
     }
 
